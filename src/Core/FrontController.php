68adb700a59862af74606f5831f56f18f4bc08e7
Paul Nikitin <top@neox.ru>
2020-01-07 00:29:51 +0300

    FIX: ссылка на репозиторий

--- src/Core/FrontController.php
+++ src/Core/FrontController.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Ideal CMS (https://idealcms.ru/)
- * @link      https://github.com/ideals/idealcms репозиторий исходного кода
+ * @link      https://github.com/idealcms/idealcms репозиторий исходного кода
  * @copyright Copyright (c) 2012-2020 Ideal CMS (https://idealcms.ru)
  * @license   https://idealcms.ru/license.html LGPL v3
  */
